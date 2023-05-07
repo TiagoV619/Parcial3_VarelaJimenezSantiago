f4e814138b4251749d488db4bc5959a06a378a68
SANTIAGO VARELA JIM�NEZ <santiagovarela314184@correo.itm.edu.co>
2023-05-07 12:10:07 -0500

    I have created the Helper folder with the IUserHelper class

---
+++ WashingCar[SantiagoVarela]/WashingCar[SantiagoVarela]/Helpers/IUserHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WashingCar_SantiagoVarela_.DAL.Entities;
+
+namespace WashingCar_SantiagoVarela_.Helpers
+{
+    public interface IUserHelper
+    {
+        Task<User> GetUserAsync(string email);
+
+        Task<IdentityResult> AddUserAsync(User user, string password);
+
+        // Task<User> AddUserAsync(AddUserViewModel addUserViewModel);
+
+        Task CheckRoleAsync(string roleName);
+
+        Task AddUserToRoleAsync(User user, string roleName);
+
+        Task<bool> IsUserInRoleAsync(User user, string roleName);
+
+        // Task<SignInResult> LoginAsync(LoginViewModel loginViewModel);
+
+        Task LogoutAsync();
+    }
+}
---
+++ WashingCar[SantiagoVarela]/WashingCar[SantiagoVarela]/Services/UserHelper.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WashingCar_SantiagoVarela_.DAL;
+using WashingCar_SantiagoVarela_.DAL.Entities;
+using WashingCar_SantiagoVarela_.Helpers;
+
+namespace WashingCar_SantiagoVarela_.Services
+{
+    public class UserHelper : IUserHelper
+    {
+        private readonly DatabaseContext _context;
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        
+        public UserHelper(DatabaseContext context, 
+            UserManager<User> userManager, 
+            RoleManager<IdentityRole> roleManager, 
+            SignInManager<User> signInManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        
+        }
+
+        public async Task<IdentityResult> AddUserAsync(User user, string password)
+        {
+            return await _userManager.CreateAsync(user,password);
+        }
+
+        public async Task AddUserToRoleAsync(User user, string roleName)
+        {
+            await _userManager.AddToRoleAsync(user, roleName);
+        }
+
+        public async Task CheckRoleAsync(string roleName)
+        {
+            bool roleExist = await _roleManager.RoleExistsAsync(roleName);                           
+
+            if (!roleExist) await _roleManager.CreateAsync(new IdentityRole { Name = roleName });
+        }
+
+        public async Task<User> GetUserAsync(string email)
+        {
+            return await _context.Users
+                .FirstOrDefaultAsync(user => user.Email == email);
+            
+        }   
+
+        public async Task<bool> IsUserInRoleAsync(User user, string roleName)
+        {
+            return await _userManager.IsInRoleAsync(user, roleName);
+        }
+
+       public Task LogoutAsync()
+        {
+            throw new NotImplementedException();
+        } 
+    }
+}
